68e4289bd2f1dc0b369d51f5392173522e591517
psanjana24 <114345160+psanjana24@users.noreply.github.com>
2022-10-29 12:28:35 +0530

    Update greatest number.py

--- greatest number.py
+++ greatest number.py
@@ -1,5 +1,5 @@
 def greatest_number():
-    a=list(map(int,input("Input the 5 numbers with spaces - ").split(' ')))
+    a=list(map(int,input("Input the 5 numbers with spaces - ").split(' ')))  
     if len(a)==5:
         flag=a[0]
         for i in a[1:]:
@@ -10,7 +10,7 @@ def greatest_number():
         print("The greatest number is",flag)
     else:
         print('You were supposed to enter 5 numbers.')
-        
+  # we r making chnges       
         
 greatest_number()
         
